b26e26fa1c6638f4a2033c7c5c2be74276a41f75
kanyaw <kanyawyousif243@gmail.com>
2024-02-13 19:50:36 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+console.log("Result of multiplication:", multiply);
 
-// Write your code here
+const random = Math.floor(Math.random() * 100) + 1; 
+console.log("Random number:", random);
+
+const num3 = 14;
+const num4 = 10;
+const mod = num3 % num4;
+console.log("Result of modulo operation:", mod);
+
+const max = Math.max(5, 10, 20, 15);
+console.log("Maximum number:", max);
